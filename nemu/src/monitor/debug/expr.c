3829167791036ed26125f4750d0a831bebb05dc1
2211849-HuiWenjing <1691888682@qq.com>
2025-03-06 13:02:54 +0800

    词法分析

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,6 +5,7 @@
  */
 #include <sys/types.h>
 #include <regex.h>
+#include <string.h>
 
 enum {
   TK_NOTYPE = 256, //空白
@@ -16,7 +17,7 @@ enum {
   TK_NUM,
   TK_REG,
   TK_HEX,//十六进制
-  TK_DEREF, //指针解引用
+  //TK_DEREF, //指针解引用
 
   TK_LEFT,
   TK_RIGHT,
@@ -49,7 +50,7 @@ static struct rule {
   {"[0-9]*",TK_NUM},
   {"\\$[a-zA-z]*[0-9]*",TK_REG},
   {"0x[0-9a-fA-F]+",TK_HEX},
-  {"\\*",TK_DEREF},
+   //{"\\*",TK_DEREF},
   {"\\(",TK_LEFT},
   {"\\)",TK_RIGHT},
   {"\\-",TK_SUB},
@@ -90,22 +91,23 @@ Token tokens[32];
 int nr_token;
 
 static bool make_token(char *e) {
-  int position = 0;
+  int position = 0; //检查字符串的位置
   int i;
-  regmatch_t pmatch;
+  regmatch_t pmatch; //存储正则表达式的匹配结果
 
-  nr_token = 0;
+  nr_token = 0; //识别token数量
 
   while (e[position] != '\0') {
     /* Try all rules one by one. */
-    for (i = 0; i < NR_REGEX; i ++) {
+    for (i = 0; i < NR_REGEX; i ++) {  //遍历正则式
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-        char *substr_start = e + position;
-        int substr_len = pmatch.rm_eo;
+        //尝试匹配第i个正则式
+        char *substr_start = e + position; //匹配的子字符串的起始位置
+        int substr_len = pmatch.rm_eo; //匹配到的子字符串长度
 
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
-        position += substr_len;
+        position += substr_len; //跳过已匹配的子字符串
 
         /* TODO: Now a new token is recognized with rules[i]. Add codes
          * to record the token in the array `tokens'. For certain types
@@ -113,7 +115,128 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
-          default: TODO();
+          case TK_NOTYPE:
+            break;
+          case TK_NUM:
+            if(nr_token >= 32)
+            {
+              printf("ERROR:too many tokens!");
+              return false;
+            }
+
+            if(substr_len>=32)
+            {
+              printf("ERROR:tokens are too long!");
+              return false;
+            }
+
+            tokens[nr_token].type = TK_NUM;
+
+            for(int j=0;j < sizeof(tokens[nr_token].str)-1 && j < substr_len;j++)
+            {
+              tokens[nr_token].str[j]=substr_start[j];
+            }
+            tokens[nr_token].str[substr_len] = "\0";
+
+            nr_token++;
+
+            break;
+          case TK_ADD:
+            tokens[nr_token].type = TK_ADD;
+            nr_token++;
+            break;
+          case TK_SUB:
+            tokens[nr_token].type = TK_SUB;
+            nr_token++;
+            break;
+          case TK_DIV:
+            tokens[nr_token].type = TK_DIV;
+            nr_token++;
+            break;
+          case TK_MUL:
+            tokens[nr_token].type = TK_MUL;
+            nr_token++;
+            break;
+          case TK_HEX:
+            if(nr_token >= 32)
+            {
+              printf("ERROR:too many tokens!");
+              return false;
+            }
+
+            if(substr_len>=32)
+            {
+              printf("ERROR:tokens are too long!");
+              return false;
+            }
+
+            tokens[nr_token].type = TK_HEX;
+
+            for(int j=0;j < sizeof(tokens[nr_token].str)-1 && j < substr_len;j++)
+            {
+              tokens[nr_token].str[j]=substr_start[j];
+            }
+            tokens[nr_token].str[substr_len] = "\0";
+
+            nr_token++;
+
+            break;
+          case TK_REG:
+            if(nr_token >= 32)
+            {
+              printf("ERROR:too many tokens!");
+              return false;
+            }
+
+            if(substr_len>=32)
+            {
+              printf("ERROR:tokens are too long!");
+              return false;
+            }
+
+
+            tokens[nr_token].type = TK_REG;
+
+            for(int j=0;j < sizeof(tokens[nr_token].str)-1 && j < substr_len;j++)
+            {
+              tokens[nr_token].str[j]=substr_start[j];
+            }
+            tokens[nr_token].str[substr_len] = "\0";
+
+            nr_token++;
+
+            break;
+          case TK_EQ:
+            tokens[nr_token].type = TK_EQ;
+            nr_token++;
+            break;
+          case TK_NEQ:
+            tokens[nr_token].type = TK_NEQ;
+            nr_token++;
+            break;
+          case TK_AND:
+            tokens[nr_token].type = TK_AND;
+            nr_token++;
+            break;
+          case TK_OR:
+            tokens[nr_token].type = TK_OR;
+            nr_token++;
+            break;
+          case TK_NOT:
+            tokens[nr_token].type = TK_NOT;
+            nr_token++;
+            break;
+          case TK_LEFT:
+            tokens[nr_token].type = TK_LEFT;
+            nr_token++;
+            break;
+          case TK_RIGHT:
+            tokens[nr_token].type = TK_RIGHT;
+            nr_token++;
+            break;
+          default: 
+            printf("not such tokens!");
+            break;
         }
 
         break;
