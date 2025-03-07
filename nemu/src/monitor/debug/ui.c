c2fb99b99968695a6cbfbc4424629ed9e7660b0d
tracer-ics2017 <tracer@njuics.org>
2025-03-07 13:13:19 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  13:13:19 up 1 day,  2:03,  1 user,  load average: 0.99, 0.38, 0.25 6d0d01b922f9910fb367458b3f91a1f77f15af8d

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -58,7 +58,7 @@ static int cmd_info(char *args)
   {
     for(int i=0;i<8;i++)
     {
-      printf("%s:0x%08x\t",reg_name(i,4),reg_l(i));
+      printf("%s:0x%08x\n",reg_name(i,4),reg_l(i));
     }
   }
   else if (strcmp(args,"w")==0)
