e9272494075fadeafd1dfe52cf7107c6867dddab
tracer-ics2017 <tracer@njuics.org>
2025-03-09 21:48:06 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  21:48:06 up  5:18,  1 user,  load average: 1.70, 1.35, 1.22 1fa24311463dc3582810fee54fa7ae583950f8b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -428,6 +428,7 @@ uint32_t eval(int p,int q)
     {
       if (strlen(tokens[p].str)==3)
       {
+        printf("reg len is 3\n");
         for (int i=0;i<8;i++)
         {
           return reg_l(i); //32位寄存器
