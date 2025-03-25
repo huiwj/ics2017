1fbb0d55bcc506d02ef61b8912b930ab8cc197e7
2211849-HuiWenjing <1691888682@qq.com>
2025-03-25 20:43:34 +0800

    logic

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -1,13 +1,24 @@
 #include "cpu/exec.h"
 
 make_EHelper(test) {
-  TODO();
+  //TODO();
+  rtl_and(&t0,&id_dest->val,&id_src->val);
+
+  rtl_update_ZFSF(&t0,id_dest->width);
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
 
   print_asm_template2(test);
 }
 
 make_EHelper(and) {
-  TODO();
+  //TODO();
+  rtl_and(&t0,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t0);
+
+  rtl_update_ZFSF(&t0,id_dest->width);
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
 
   print_asm_template2(and);
 }
@@ -25,28 +36,44 @@ make_EHelper(xor) {
 }
 
 make_EHelper(or) {
-  TODO();
+  //TODO();
+  rtl_or(&t0,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t0);
+
+  rtl_update_ZFSF(&t0,id_dest->width);
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
 
   print_asm_template2(or);
 }
 
 make_EHelper(sar) {
-  TODO();
+  //TODO();
   // unnecessary to update CF and OF in NEMU
+  rtl_sext(&t0,&id_dest->val,id_dest->width);
+  rtl_sar(&t0,&t0,&id_src->val);
+  operand_write(id_dest,&t0);
+  rtl_update_ZFSF(&t0,id_dest->width);
 
   print_asm_template2(sar);
 }
 
 make_EHelper(shl) {
-  TODO();
+  //TODO();
   // unnecessary to update CF and OF in NEMU
+  rtl_shl(&t0,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t0);
+  rtl_update_ZFSF(&t0,id_dest->width);
 
   print_asm_template2(shl);
 }
 
 make_EHelper(shr) {
-  TODO();
+  //TODO();
   // unnecessary to update CF and OF in NEMU
+  rtl_shr(&t0,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t0);
+  rtl_update_ZFSF(&t0,id_dest->width);
 
   print_asm_template2(shr);
 }
@@ -60,7 +87,9 @@ make_EHelper(setcc) {
 }
 
 make_EHelper(not) {
-  TODO();
+  //TODO();
+  rtl_not(&id_dest->val);
+  operand_write(id_dest,&t0);
 
   print_asm_template1(not);
 }
