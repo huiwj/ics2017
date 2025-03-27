d70a1205f16359c0f7dc3601b49e7d14d2ae113e
tracer-ics2017 <tracer@njuics.org>
2025-03-27 20:19:58 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:19:58 up  6:08,  1 user,  load average: 0.28, 0.31, 0.26 12e7b29318c7f38c7a98b7424ecf2598400d1642

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -7,9 +7,11 @@ make_EHelper(mov) {
 
 make_EHelper(push) {
   //TODO();
-  rtl_push(&id_dest->val);
+  rtl_sext(&t0,&id_dest->val,id_dest->width);
+  rtl_push(&t0);
 
   print_asm_template1(push);
+  printf("push:esp: esp=0x%08x,val=0x%08x\n",cpu.esp,id_dest->val);
 }
 
 make_EHelper(pop) {
