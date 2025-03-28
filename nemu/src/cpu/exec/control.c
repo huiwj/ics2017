851dca2bfb349786b6a37140e1eab7ff8c6aacef
tracer-ics2017 <tracer@njuics.org>
2025-03-28 11:45:20 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:45:19 up  2:23,  1 user,  load average: 1.95, 1.59, 1.50 cb3bae9deae85bf2d49d3799f7eea288797fdb45

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -11,6 +11,7 @@ make_EHelper(jmp) {
 make_EHelper(jcc) {
   // the target address is calculated at the decode stage
   uint8_t subcode = decoding.opcode & 0xf;
+  printf("subcode:%d\n",subcode);
   rtl_setcc(&t2, subcode);
   decoding.is_jmp = t2;
   printf("[jcc] ZF=%d,taken=%d,target=0x%08x\n",
