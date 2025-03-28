ef1c1ffc366fc0764e273c2c7e8b4615ea170e8f
tracer-ics2017 <tracer@njuics.org>
2025-03-28 12:06:22 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  12:06:22 up  2:44,  1 user,  load average: 1.89, 1.67, 1.62 bb1ebfa41525809aa5995108d17640bc8e9d181

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -63,7 +63,7 @@ make_EHelper(cmp) {
   rtl_msb(&t0,&t0,id_dest->width);//取最高位
   rtl_set_OF(&t0);
 
-  rtl_update_ZFSF(&t0,id_dest->width);
+  rtl_update_ZFSF(&t0,4);
 
   print_asm_template2(cmp);
 }
