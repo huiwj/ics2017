79998f516ffa44046f03e16a28946db037b9c212
tracer-ics2017 <tracer@njuics.org>
2025-03-28 15:23:48 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:23:48 up 54 min,  1 user,  load average: 0.73, 0.38, 0.19 6610f5de5fb67817651320687a39464c284421b3

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -88,7 +88,8 @@ make_EHelper(setcc) {
 
 make_EHelper(not) {
   //TODO();
-  rtl_not(&id_dest->val);
+  t0 = id_dest->val;
+  rtl_not(&t0);
   operand_write(id_dest,&t0);
 
   print_asm_template1(not);
