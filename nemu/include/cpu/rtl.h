3c0126f29baa7858a528d0beddbb9da1ac3f29d1
tracer-ics2017 <tracer@njuics.org>
2025-03-28 19:25:28 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:25:28 up 24 min,  1 user,  load average: 0.61, 0.29, 0.20 e817969146f80f5f7827f9a74cd54c43655c8ad4

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -140,8 +140,8 @@ static inline void rtl_not(rtlreg_t* dest) {
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   //TODO();
-  uint32_t shift = (sizeof(rtlreg_t)-width)*8;
-  *dest = (*src1 << shift) >> shift;
+  int shift = (8*(4-width));
+  *dest = ((int32_t)(*src1<<shift))>>shift;
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
