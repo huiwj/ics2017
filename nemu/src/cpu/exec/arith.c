4fbb6b67f542b15b8bda504d11bc1025301b276f
tracer-ics2017 <tracer@njuics.org>
2025-03-28 18:35:58 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:35:58 up  3:10,  1 user,  load average: 0.84, 0.44, 0.24 55e9f6ec85d9b2a466585c0ff7a605e412de2da3

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -61,11 +61,24 @@ make_EHelper(cmp) {
 
   //有符号溢出
   rtlreg_t res = t0;
+  /* 
   rtl_xor(&t0,&t1,&t2);//t0=dest^src
   rtl_xor(&t1,&t1,&res);//t1=dest^res
   rtl_and(&t0,&t0,&t1);//t0=t0 & t1
   rtl_msb(&t0,&t0,id_dest->width);//取最高位
   rtl_set_OF(&t0);
+  */
+  rtl_msb(&t1,&t1,id_dest->width);
+  rtl_msb(&t2,&t2,id_dest->width);
+  rtl_msb(&t0,&res,id_dest->width);
+
+  rtl_xor(&t3,&t1,&t2);
+  rtl_xor(&t2,&t0,&t1);
+  rtl_xor(&t3,&t3,&t2);
+
+  rtl_set_OF(&t3);
+
+
   printf("OF:%d\n",cpu.OF);
 
 
