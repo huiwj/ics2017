8a13512622e55e7f817dc960d4d4575ddc70c2dc
tracer-ics2017 <tracer@njuics.org>
2025-03-30 00:46:35 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:46:35 up  1:51,  1 user,  load average: 0.36, 0.34, 0.28 289d7e4ee25c2a5363adfbf3fbc3e1fedb57a3f0

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,4 +1,5 @@
 #include "nemu.h"
+#include "device/mmio.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -12,11 +13,22 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
+  int mmio_id = is_mmio(addr);
+  if(mmio_id>=0)
+  {
+    return mmio_read(addr,len,mmio_id);
+  }
   return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+  int mmio_id = is_mmio(addr);
+  if(mmio_id>=0)
+  {
+    return mmio_write(addr,data,len,mmio_id);
+  }
+  else
+    memcpy(guest_to_host(addr), &data, len);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
