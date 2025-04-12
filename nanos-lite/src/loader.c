7f2b74216ecf037949b1d716c59088b7a0e4c0a7
tracer-ics2017 <tracer@njuics.org>
2025-04-12 18:09:09 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:09:09 up  1:53,  1 user,  load average: 0.33, 0.23, 0.14 866565096edb3f9865db01a87cff375ae599441e

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,15 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern size_t get_ramdisk_size();
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  //TODO();
+  //获取大小
+  size_t ramdisk_size = get_ramdisk_size();
+  //读取到目标地址
+  ramdisk_read(DEFAULT_ENTRY,0,ramdisk_size);
+  //返回入口地址
   return (uintptr_t)DEFAULT_ENTRY;
 }
