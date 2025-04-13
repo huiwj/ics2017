b79cb9d90cea423376d63e8e225eae69d1f70c36
tracer-ics2017 <tracer@njuics.org>
2025-04-13 17:18:40 +0800

    >  run 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:18:40 up 23:53,  1 user,  load average: 0.38, 0.30, 0.27 c7ad915bd48ac922e82af885bc5dff216498adee

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+extern _RegSet* do_syscall(_RegSet *r);
+
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL: return do_syscall(r);
