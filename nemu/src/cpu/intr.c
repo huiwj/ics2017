2504aa90ca4f9cb577215955ae012698f3fac6ef
tracer-ics2017 <tracer@njuics.org>
2025-04-13 15:52:35 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:52:35 up 22:27,  1 user,  load average: 0.84, 0.36, 0.24 ed1cc8703ffbeea8f41b039a1f701014e78d3df6

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -9,27 +9,25 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   //TODO();
   //计算中断描述符在IDT中地址
   uint32_t idt_base = cpu.idtr.base;
+  uint32_t idt_limit = cpu.idtr.limit;
   uint32_t idt_entry_addr = idt_base + NO *8;
 
+  assert(idt_entry_addr+7<=idt_base + idt_limit);
   //读取IDT表项
   uint32_t low = vaddr_read(idt_entry_addr,4);
   uint32_t high = vaddr_read(idt_entry_addr+4,4);
-
   //提取offset
   uint32_t offset_low = low & 0xFFFF;
-  uint32_t offset_high = (high >> 16)&0xFFFF;
-  uint32_t handler_addr = (offset_high << 16)|offset_low;
-
+  uint32_t offset_high = high&0xFFFF0000;
+  uint32_t handler_addr = offset_high |offset_low;
   //压栈
   cpu.esp -=4;
   vaddr_write(cpu.esp,4,cpu.eflags);
-
-  cpu.esp-=4;
-  vaddr_write(cpu.esp,4,ret_addr);
-
   cpu.esp-=4;
   vaddr_write(cpu.esp,4,cpu.CS);
-
+  cpu.esp-=4;
+  vaddr_write(cpu.esp,4,ret_addr);
+  
   //设置跳转指令
   cpu.eip = handler_addr;
   cpu.CS = 8;
