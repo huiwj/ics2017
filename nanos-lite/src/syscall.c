6fff8eb841fb9afda72ad9277a143999f0aa7ec3
tracer-ics2017 <tracer@njuics.org>
2025-04-13 21:27:56 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:27:56 up 1 day,  4:02,  1 user,  load average: 0.58, 0.39, 0.19 5f405178299f8874f52bd20347316032b2837cb3

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "syscall.h"
 
+extern ssize_t fs_write(int fd,const void *buf,size_t len);
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -14,6 +15,9 @@ _RegSet* do_syscall(_RegSet *r) {
       break;
     case SYS_exit:
       _halt(SYSCALL_ARG2(r));
+      break;
+    case SYS_write:
+      SYSCALL_ARG1(r)=fs_write(SYSCALL_ARG2(r),(void*)SYSCALL_ARG3(r),SYSCALL_ARG4(r));
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
