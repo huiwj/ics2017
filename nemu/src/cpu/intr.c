f425fcd3732b1a1f6790827ca417c1d8a5853b9a
tracer-ics2017 <tracer@njuics.org>
2025-04-13 16:24:16 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:24:16 up 22:58,  1 user,  load average: 0.73, 0.39, 0.22 fb1cde964dc235141a9a0a949db728993aacf4d

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -27,6 +27,8 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   vaddr_write(cpu.esp,4,cpu.CS);
   cpu.esp-=4;
   vaddr_write(cpu.esp,4,ret_addr);
+
+  printf("raise_intr:NO=0x%x,handler=0x%x\n",NO,handler_addr);
   
   //设置跳转指令
   decoding.jmp_eip = handler_addr;
