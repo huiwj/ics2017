1461a9fdd356919c71634b0c2ed43efaf5eaaedd
tracer-ics2017 <tracer@njuics.org>
2025-04-14 17:14:03 +0800

    >  run 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:14:03 up 1 day,  7:31,  1 user,  load average: 0.26, 0.58, 0.53 72db925492bbe814d84f3dd745bbd06a7748d758

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -19,10 +19,9 @@ _RegSet* do_syscall(_RegSet *r) {
       break;
     case SYS_write:
       SYSCALL_ARG1(r)=fs_write(a[1],(void*)a[2],a[3]);
+      Log("sys_write:fd %d len %d",a[1],a[3]);
       break;
-    case SYS_brk:
-      SYSCALL_ARG1(r)=0;
-      break;
+    
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
