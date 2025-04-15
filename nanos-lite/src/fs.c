041e9fdb8b54bf858bbadc0a513660c4adcd723f
tracer-ics2017 <tracer@njuics.org>
2025-04-15 17:09:42 +0800

    >  run 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:09:42 up  8:14,  1 user,  load average: 0.43, 0.32, 0.28 336c0d9656b73aba5f1c645cc9b23898cb57da6b

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -55,9 +55,10 @@ ssize_t fs_write(int fd,const void *buf,size_t len)
       
       return len;
     case FD_FB:
-      fb_write(buf,file_table[fd].open_offset,len);
-      file_table[fd].open_offset += len;
-      return len;
+      //fb_write(buf,file_table[fd].open_offset,len);
+      //file_table[fd].open_offset += len;
+      //return len;
+      return 0;
     default:
       ramdisk_write((void*)buf,file_table[fd].disk_offset + file_table[fd].open_offset,len);
       file_table[fd].open_offset += len;
@@ -89,13 +90,15 @@ ssize_t fs_read(int fd,void *buf,size_t len)
   if(fd==FD_STDIN || fd == FD_STDOUT || fd == FD_STDERR) return 0;
   if(fd == FD_DISPINFO) //固定内容文件使用偏移读取
   {
-    dispinfo_read(buf,file_table[fd].open_offset,len);
-    file_table[fd].open_offset += len;
-    return len;
+    //dispinfo_read(buf,file_table[fd].open_offset,len);
+    //file_table[fd].open_offset += len;
+    //return len;
+    return 0;
   }
   if(fd == FD_EVENTS)//不断产生数据的设备，返回长度由events_read决定
   {
-    return events_read(buf,len);
+    //return events_read(buf,len);
+    return 0;
   }
   //普通文件
   {
