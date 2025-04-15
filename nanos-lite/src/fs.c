b3a3d36b11b41e5bd7b1bfd2570ed94e6dfa9e9a
tracer-ics2017 <tracer@njuics.org>
2025-04-15 12:12:43 +0800

    >  run 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  12:12:43 up  3:31,  1 user,  load average: 0.60, 0.23, 0.18 8c0c2a743dd506847ef5bed6f83f296f99f6743

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,9 +1,16 @@
 #include "fs.h"
 
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern void ramdisk_write(const void *buf, off_t offset, size_t len);
+extern size_t events_read(void *buf, size_t len);
+extern void dispinfo_read(void *buf, off_t offset, size_t len);
+extern void fb_write(const void *buf, off_t offset, size_t len);
+
 typedef struct {
   char *name;
   size_t size;
   off_t disk_offset;
+  off_t  open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
@@ -28,9 +35,16 @@ void init_fs() {
 
 ssize_t fs_write(int fd,const void *buf,size_t len)
 {
-  //ssize_t size = file_table[fd].size;
+  ssize_t size = file_table[fd].size;
+
+  if(file_table[fd].open_offset + len > size)
+  {
+    len = size - file_table[fd].open_offset;
+  }
   switch(fd)
   {
+    case FD_STDIN:
+      return 0;
     case FD_STDOUT:
     case FD_STDERR:
       Log("sys_write:fd %d len %d",fd,len);
@@ -40,8 +54,89 @@ ssize_t fs_write(int fd,const void *buf,size_t len)
       }
       
       return len;
+    case FD_FB:
+      fb_write(buf,file_table[fd].open_offset,len);
+      file_table[fd].open_offset += len;
+      return len;
     default:
-      return 0;
+      ramdisk_read((void*)buf,file_table[fd].disk_offset + file_table[fd].open_offset,len);
+      file_table[fd].open_offset += len;
+      return len;
 
   }
+}
+
+int fs_open(const char *pathname,int flags, int mode)
+{
+  for(int i=0;i<sizeof(file_table)/sizeof(file_table[0]);i++)
+  {
+    if(strcmp(file_table[i].name,pathname)==0)
+    {
+      file_table[i].open_offset=0;
+      return i;
+    }
+  }
+  panic("File '%s' not found",pathname);
+  return -1;
+}
+
+ssize_t fs_read(int fd,void *buf,size_t len)
+{
+  if(fd==FD_STDIN || fd == FD_STDOUT || fd == FD_STDERR) return 0;
+  if(fd == FD_DISPINFO) //固定内容文件使用偏移读取
+  {
+    dispinfo_read(buf,file_table[fd].open_offset,len);
+    file_table[fd].open_offset += len;
+    return len;
+  }
+  if(fd == FD_EVENTS)//不断产生数据的设备，返回长度由events_read决定
+  {
+    return events_read(buf,len);
+  }
+  //普通文件
+  {
+    ramdisk_read((void*)buf,file_table[fd].disk_offset + file_table[fd].open_offset,len);
+    file_table[fd].open_offset += len;
+    return len;
+  }
+
+}
+
+int fs_close(int fd)
+{
+  return 0;
+}
+
+size_t fs_filesz(int fd)
+{
+  return file_table[fd].size;
+}
+
+off_t fs_lseek(int fd,off_t offset, int whence)
+{
+  Finfo *f = &file_table[fd];
+  off_t new_offset = 0;
+
+  switch (whence)
+  {
+  case SEEK_SET:
+    new_offset = offset;
+    break;
+  case SEEK_CUR:
+    new_offset = f->open_offset + offset;
+    break;
+  case SEEK_END:
+    new_offset = f->size + offset;
+    break;
+  default:
+    return -1;
+  }
+
+  if(new_offset > f->size)
+  {
+    return -1;
+  }
+
+  f->open_offset = new_offset;
+  return new_offset;
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -4,13 +4,22 @@
 
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
+extern int fs_close(int fd);
+extern ssize_t fs_read(int fd,void *buf,size_t len);
+extern size_t fs_filesz(int fd);
+extern int fs_open(const char *pathname,int flags, int mode);
 
 uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
   //获取大小
-  size_t ramdisk_size = get_ramdisk_size();
+  int fd = fs_open(filename,0,0);
+  size_t size = fs_filesz(fd);
+  fs_read(fd,(void*)DEFAULT_ENTRY,size);
+  fs_close(fd);
+  
+  //size_t ramdisk_size = get_ramdisk_size();
   //读取到目标地址
-  ramdisk_read(DEFAULT_ENTRY,0,ramdisk_size);
+ // ramdisk_read(DEFAULT_ENTRY,0,ramdisk_size);
   //返回入口地址
   return (uintptr_t)DEFAULT_ENTRY;
 }
