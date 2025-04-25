ed2a55d9f5068cb69011f7bf6808ec80b56f4a98
tracer-ics2017 <tracer@njuics.org>
2025-04-25 16:41:29 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:41:29 up 12:06,  1 user,  load average: 1.09, 0.71, 0.57 6a1e6f6d323788bd10a265f25d7bff1aad8c18da

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,7 +22,7 @@ size_t events_read(void *buf, size_t len) {
   {
     sprintf(buf,"%s %s\n",is_key_down ? "kd" : "ku",keyname[key]);
     Log("Key Event: %s", buf);
-    //assert(0 && "key bord");
+    assert(0 && "key bord");
   }
   else
   {
