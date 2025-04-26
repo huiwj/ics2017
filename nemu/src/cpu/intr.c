d29e36d3547df9bc103f76d0b02b6cbb985ba998
tracer-ics2017 <tracer@njuics.org>
2025-04-26 15:53:39 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:53:39 up  4:31,  1 user,  load average: 0.36, 0.12, 0.12 4cf516a323322799b6d8f7947470422d70a42aae

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -13,6 +13,12 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   uint32_t idt_entry_addr = idt_base + NO *8;
 
   assert(idt_entry_addr+7<=idt_base + idt_limit);
+
+  //压栈
+  rtl_push(&cpu.eflags);
+  rtl_push(&cpu.CS);
+  rtl_push(&ret_addr);
+  
   //读取IDT表项
   uint32_t low = vaddr_read(idt_entry_addr,4);
   uint32_t high = vaddr_read(idt_entry_addr+4,4);
@@ -20,10 +26,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   uint32_t offset_low = low & 0x0000FFFF;
   uint32_t offset_high = high &0xFFFF0000;
   uint32_t handler_addr = offset_high |offset_low;
-  //压栈
-  rtl_push(&cpu.eflags);
-  rtl_push(&cpu.CS);
-  rtl_push(&ret_addr);
+  
   //printf("raise_intr:NO=0x%x,handler=0x%x\n",NO,handler_addr);
   //设置跳转指令
   decoding.jmp_eip = handler_addr;
