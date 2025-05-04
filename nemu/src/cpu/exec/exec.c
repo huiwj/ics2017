490bf7db88f50537b9f7a834a2a9666f7c2f7640
tracer-ics2017 <tracer@njuics.org>
2025-05-05 00:48:42 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:48:42 up  3:07,  1 user,  load average: 1.04, 0.43, 0.35 83ebc534c7142bf0d7dcf9d86acc5ba76dc2b552

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -151,7 +151,7 @@ opcode_entry opcode_table [512] = {
   /* 0x24 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x28 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x2c */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x30 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x30 */	IDEX(E2G,mov_cr2r), EMPTY, IDEX(E2G,mov_r2cr), EMPTY,
   /* 0x34 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x38 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x3c */	EMPTY, EMPTY, EMPTY, EMPTY,
