be183e02850599f73adc65697688023232488a51
tracer-ics2017 <tracer@njuics.org>
2025-05-06 14:56:36 +0800

    >  run 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  14:56:36 up 10:18,  1 user,  load average: 0.59, 0.63, 0.59 9ed6b69ff62cfc0587f8f64df74be1d5789daaba

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -30,7 +30,7 @@ _RegSet* do_syscall(_RegSet *r) {
       break;
     case SYS_brk:
       //Log("[sys_brk] requested program break:%p",(void *)a[1]);
-      SYSCALL_ARG1(r)=0;
+      SYSCALL_ARG1(r)=sys_brk((void*)SYSCALL_ARG2(r));
       break;
     case SYS_write:
       SYSCALL_ARG1(r)=fs_write(a[1],(void*)a[2],a[3]);
