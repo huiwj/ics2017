95d357e6f92cae1a4622a74225c0936af4fe5529
tracer-ics2017 <tracer@njuics.org>
2025-05-10 20:11:18 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:11:18 up  1:20,  1 user,  load average: 0.94, 0.36, 0.25 953e561cdda9b1fcefcfa5483c418f641b3c3c8d

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -14,6 +14,9 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
       //do_syscall(r);
       return schedule(r);
       break;
+    case _EVENT_IRQ_TIME:
+      Log("IRQ_TIME");
+      return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -16,6 +16,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
 
   //压栈
   rtl_push(&cpu.eflags);
+  cpu.IF = 0;
   rtl_push(&cpu.CS);
   rtl_push(&ret_addr);
   
@@ -34,4 +35,5 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
 }
 
 void dev_raise_intr() {
+  cpu.INTR = true;
 }
