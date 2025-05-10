3e93c390edb1290b455051e7f1918aa9e949598c
tracer-ics2017 <tracer@njuics.org>
2025-05-10 18:17:20 +0800

    >  run 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:17:20 up 12:41,  1 user,  load average: 0.71, 0.29, 0.22 e60584c9bddb9132aa64381ff6672bd886886

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -7,11 +7,13 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL: 
       do_syscall(r);
+      return schedule(r);
       break;
     case _EVENT_TRAP:
       Log("Trap : 0x81");
-      do_syscall(r);
-      return schedule(r);
+      //do_syscall(r);
+      //return schedule(r);
+      break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
