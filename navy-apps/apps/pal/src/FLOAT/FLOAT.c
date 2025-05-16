9317de09909c6af1f4d0793d39045dee5e16ef87
tracer-ics2017 <tracer@njuics.org>
2025-05-16 17:03:56 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:03:56 up  3:09,  1 user,  load average: 0.68, 1.04, 0.69 8c37dc9c81051e25b887804188b3ae7ac2427c23

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -3,13 +3,41 @@
 #include <assert.h>
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+
+  return ((int64_t)a * (int64_t)b)>>16;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+  //assert(0);
+
+  int is_neg = 0;//记录符号
+  if(a<0)
+  {
+    a=-a;
+    is_neg ^= 1;
+  }
+  if(b<0)
+    {
+      b=-b;
+      is_neg ^= 1;
+    }
+
+//初始整数
+  int quotient = a/b;
+  int remainder = a%b;
+//扩展小数
+for(int i=0;i<16;++i)
+{
+  quotient <<=1;
+  remainder <<=1;
+  if(remainder >= b)
+  {
+    remainder -= b;
+    quotient |= 1;
+  }
+}
+
+  return is_neg? -quotient : quotient;
 }
 
 FLOAT f2F(float a) {
@@ -23,13 +51,38 @@ FLOAT f2F(float a) {
    * performing arithmetic operations on it directly?
    */
 
-  assert(0);
-  return 0;
+  //assert(0);
+
+  uint32_t bits;
+  memcpy(&bits,&a,sizeof(bits));
+
+  //拆解IEEE 754结构
+  int sign = bits >> 31;
+  int exp = ((bits>>23) & 0xFF)-127;//减去bias
+  uint32_t frac = bits & 0x7FFFFF; //原始小数
+  uint32_t mantissa = frac | 0x80000000; //隐藏位1
+
+  int64_t value = (int64_t)mantissa; //32位定点格式
+
+  if(exp >= 0)
+  {
+    value <<= exp; //左移放大
+  }
+  else
+  {
+    value >>= -exp; //右移减小
+  }
+
+  //小数点左移16位
+  value = (value * (1<<16))>>23;
+  if(sign) value = -value;
+  
+  return (int32_t)value;
 }
 
 FLOAT Fabs(FLOAT a) {
-  assert(0);
-  return 0;
+  
+  return (a > 0)?a:-a;
 }
 
 /* Functions below are already implemented */
