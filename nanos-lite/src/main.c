5a1f757c2469c0dfd456b72ff39b6da31eff74ab
tracer-ics2017 <tracer@njuics.org>
2025-05-16 17:18:00 +0800

    >  run 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:18:00 up  3:23,  1 user,  load average: 0.61, 0.80, 0.78 bf638a18e9df2a06d0f05618d754e040e1f744f7

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -31,9 +31,9 @@ int main() {
 
   init_fs();
 
-  //uint32_t entry = loader(NULL, "/bin/pal");
-  //((void (*)(void))entry)();
-  load_prog("/bin/pal");
+  uint32_t entry = loader(NULL, "/bin/pal");
+  ((void (*)(void))entry)();
+  //load_prog("/bin/pal");
   
   //load_prog("/bin/hello");
   //load_prog("/bin/videotest");
