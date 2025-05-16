a904eab9039342a54cdeb30b4d5d6a3c6c048a27
tracer-ics2017 <tracer@njuics.org>
2025-05-16 20:17:16 +0800

    >  compile 2211849 hui Linux hui-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:17:16 up  6:23,  1 user,  load average: 0.77, 0.71, 0.69 31dde2aa3339ebc35591f01260c83ae1abfc4e7d

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -158,6 +158,12 @@ make_DHelper(I_E2G) {
   decode_op_I(eip, id_src, true);
 }
 
+make_DHelper(I_G2E) {
+  decode_op_rm(eip, id_dest, true, id_src2, true);
+  id_src->width =1;
+  decode_op_I(eip, id_src, true);
+}
+
 /* Eb <- Ib
  * Ev <- Iv
  */
